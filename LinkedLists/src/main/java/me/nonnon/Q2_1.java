8e6cb46d9b7a3130a4721c875cba5c772e899e28
Ha Hwang <spyderthing@gmail.com>
2020-06-18 23:53:30 -0400

    Added Q2_1 class and removeDuplicates method

---
+++ LinkedLists/src/main/java/me/nonnon/Q2_1.java
@@ -0,0 +1,25 @@
+package me.nonnon;
+
+import java.util.HashSet;
+
+public class Q2_1 {
+	// Write code to remove duplicates from an unsorted linked list. How would you solve this problem if a 
+	// temporary buffer is not allowed?
+	
+	public void removeDuplicates(SinglyLinkedList list) {
+		HashSet<Integer> set = new HashSet<Integer>();
+		Node prev = null;
+		Node ptr = list.head;		
+		while (ptr != null) {
+			if (set.contains(ptr.data)) {
+				prev.next = ptr.next;
+			} else {
+				set.add(ptr.data);
+				prev = ptr;
+			}
+			ptr = ptr.next;
+		}
+		
+	}
+
+}
